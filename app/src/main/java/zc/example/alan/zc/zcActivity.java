34690deaeae4c1a5398a4920b44a011afa91d692
nanghua339 <nanghua339@163.com>
2018-06-28 02:13:18 +0800

    Update zcActivity.java

--- app/src/main/java/zc/example/alan/zc/zcActivity.java
+++ app/src/main/java/zc/example/alan/zc/zcActivity.java
@@ -1,1 +1,27 @@
-package zc.example.alan.zc;import android.support.v7.app.AppCompatActivity;import android.os.Bundle;public class zcActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_zc);    }    public void develop(){        int a = 2;    }    public void feature_2(){        int k =1;        int kk =1;    }    public void feature_3(){        int b =1;        int c =1;    }}
+package zc.example.alan.zc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class zcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zc);
+    }
+    public void develop(){
+        int a = 2;
+
+    }
+    public void feature_2(){
+        int k =1;
+        int kk =1;
+    }
+    public void feature_3(){
+        int b =1;
+        int c =1;
+        int zx = 339;
+    }
+
+}
